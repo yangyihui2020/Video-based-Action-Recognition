bf3ca6941e45cd1b27f145454e6b73e8b3906167
Universe Star <118490975+yxzwan@users.noreply.github.com>
2024-11-15 14:08:12 +0800

    add keyframes-extracting module in pytorch version.

---
+++ Video-based-Action-Recognition-Pytorch/extract_keyframes.sh
@@ -0,0 +1,7 @@
+python extract.py \
+--video_path "./test_videos/20241025_105625.mp4" \
+--output_dir "./Output" \
+--output_video_name "output.mp4" \
+--frame_rate 5 \
+--split \
+--clip_duration 5 \
